244aa9a56c5d0335bb7ebce0ccdbe220a830f21f
s13814 <s13814@pjwstk.edu.pl>
2019-10-29 14:38:58 +0100

    Dodano przycisk i alert dodawania produktu.

--- app/src/main/java/com/example/mp1/ProductListActivity.java
+++ app/src/main/java/com/example/mp1/ProductListActivity.java
@@ -1,5 +1,6 @@
 package com.example.mp1;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
@@ -8,7 +9,11 @@ import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.example.mp1.DB.Product;
 import com.example.mp1.DB.ProductDB;
@@ -48,6 +53,42 @@ public class ProductListActivity extends AppCompatActivity {
         });
     }
 
+    public void clickAdd(View view){
+        AlertDialog.Builder ad = new AlertDialog.Builder(ProductListActivity.this);
+        ad.setTitle("Add product");
+        ad.setMessage("Enter new product");
+
+        ad.setPositiveButton("Add",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+        ad.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+        LinearLayout layout = new LinearLayout(ProductListActivity.this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        EditText name = new EditText(ProductListActivity.this);
+        name.setHint("Name");
+
+        EditText price = new EditText(ProductListActivity.this);
+        price.setHint("Price");
+
+        layout.addView(name);
+        layout.addView(price);
+
+        ad.setView(layout, 50, 0, 50, 0);
+        ad.show();
+    }
     /*private List<Product> getProducts(){
         LiveData<List<Product>> lp = new LiveData<List<Product>>();
         lp = db.productDao().getAllProducts();

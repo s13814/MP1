cffd655efe399c517bcf395581e3a6a6be56f3dd
s13814 <s13814@pjwstk.edu.pl>
2019-11-12 04:42:08 +0100

    ...

--- app/src/main/java/com/example/mp1/MainActivity.java
+++ app/src/main/java/com/example/mp1/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
